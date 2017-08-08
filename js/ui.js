2aaaf96efeb9f842f7cc1a3d0259c20d4f8ce332
fhaque <frhaque@gmail.com>
2017-08-08 15:50:18 -0400

    done without animations/transitions

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,13 @@
 $('#mainMenuBarIcon').click(() =>{
 	$('#navBar').toggleClass('navBar--viewable');
 })
+
+//Modified details selector code from
+//https://stackoverflow.com/questions/16751345/automatically-close-all-the-other-details-tags-after-opening-a-specific-detai
+$('.detailsContainer details').click(function(e) {
+	$('.detailsContainer details').not(this).removeAttr("open");
+
+	if ($(this).attr("open") === true) {
+		$(this).removeAttr("open");
+	}
+});
